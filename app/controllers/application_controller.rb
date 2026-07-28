b0d23e542eb683c15befd7e9e30f627a4d60803d
NgoTung1 <uaaa2511@gmail.com>
2026-07-28 11:31:47 +0700

    feature: global layout - header(60%), footer(100%)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  def current_user
+    if cookies.encrypted[:user_id]
+      @current_user ||= User.find_by(id: cookies.encrypted[:user_id])
+    end
+  end
+
+  def logged_in?
+    current_user.present?
+  end
 end
