6f324abe4bc88b649a760522425f28a36e056dba
NgoTung1 <uaaa2511@gmail.com>
2026-08-19 11:21:11 +0700

    change port for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     domain: 'gmail.com',
     user_name: ENV['GMAIL'],
     password: ENV['GMAIL_PASSWORD'],
